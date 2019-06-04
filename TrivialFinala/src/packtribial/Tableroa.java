951bd4585a6a5d7dc6373b07ceb1f9f7088581bc
GySyX <GySyX@LAPTOP-K6JCCTH2>
2019-06-04 21:50:47 +0200

    Tableroa comentado y ListaGalderak sin el blocazo de código comentado

--- TrivialFinala/src/packtribial/Tableroa.java
+++ TrivialFinala/src/packtribial/Tableroa.java
@@ -9,12 +9,12 @@ import java.util.Scanner;
 
 public class Tableroa {
 	//atributuak
-	private Laukia[][]	lehenengoBidea;// Barneko 6 bideetan
-	private int			errenkadaKop=0;
-	private int			zutabeKop=0;
-	private Laukia[]	bigarrenBidea;// Kanpoko bidea, zirkulu bat
-	private	int			posizioZirkuluan=0; 		
-	private static Tableroa nireTableroa=null;
+	private Laukia[][]	lehenengoBidea;			// Barneko 6 bideetan
+	private int			errenkadaKop=0;			// Sei bideetako zein
+	private int			zutabeKop=0;			// Bidearen zein laukian
+	private Laukia[]	bigarrenBidea;			// Kanpoko bidea, zirkulu bat
+	private	int			posizioZirkuluan=0;	 	// Bidearen zein laukian		
+	private static Tableroa nireTableroa=null;	
 
 	//eraikitazilea
 	private Tableroa() {
@@ -35,19 +35,18 @@ public class Tableroa {
 	public JokalariaAldaketak fitxaMugitu(boolean pHasierakoPasilloan, int pPosizioaErrenkada, int pPosizioaZutabea,
 			ListaGaztatxoak pPuntuak, int pAteratakoa) {
 		
-		//Kontuz posizioaErrenkadarekin
-		
+
 		//Jokalariaren informazioa eguneratzeko aldagai lokalak
 		
 		boolean garaileaDa				= false;
 		boolean hasierakoPasilloan		= pHasierakoPasilloan;
 		int		egungoZutabea			= pPosizioaZutabea;
 		int		egungoErrenkada			= pPosizioaErrenkada;
-		String  erantzuna				= "okerra";					//jokalariaren puntuak kontrolatzeko
+		String  erantzuna				= "okerra";					//ListaGaztatxoak honen balioa kudeatuko du
 		int		kont					= 0;
 		
 		
-		if(!hasierakoPasilloan){
+		if(!hasierakoPasilloan){ // Zirkuluan dagoela jokalaria
 			
 			egungoZutabea	= this.mugituZirkuluan(egungoZutabea, pAteratakoa);
 			erantzuna		= this.bigarrenBidea[egungoZutabea].galderaFormulatu();
@@ -55,32 +54,32 @@ public class Tableroa {
 			//jokalariaren puntuak eguneratzen
 			pPuntuak.gehituGaztatxoa(erantzuna);
 				 
-			if (pPuntuak.zenbatGaztatxo() == 6){
+			if (pPuntuak.zenbatGaztatxo() == 6){ //6. puntua lortzerakoan pasilloaren sarreran bertan egongo da
 					 
-				hasierakoPasilloan	= true;
+				hasierakoPasilloan	= true; // Pasilloan mugitu ahal izateko behar duen salataria
 				
-				//downcasting-a erabiliz
+				//downcasting-a erabiliz, pasilloan mugitzeko behar dituen koordenatuak jasotzeko
 				egungoErrenkada		= ((LaukiGaztaduna)this.bigarrenBidea[egungoZutabea]).getErrenkada();
 				egungoZutabea		= ((LaukiGaztaduna)this.bigarrenBidea[egungoZutabea]).getZutabeaPasilloan();
 			}
 			
 		}
 		
-		else{
+		else{// Pasillo baten barruan dagoela jokalaria
 			
 			if (pPuntuak.zenbatGaztatxo() == 6){
-				//pasilloaren barnean dago eta jadanik sartu da
+				//pasilloaren barnean dago, sei puntu dituela erdialdeko laukira mugitzeko asmoarekin
 				
 				
 				egungoZutabea	= egungoZutabea - pAteratakoa;
 				
-				if (egungoZutabea < 0){
+				if (egungoZutabea < 0){ //Erdialdera behar duen baino emaitza handiagoa ateratzen badu, onartuko da
 					
-					egungoZutabea = 0;
+					egungoZutabea = 0; 
 				}
 				erantzuna		= this.lehenengoBidea[egungoErrenkada][egungoZutabea].galderaFormulatu();
 				
-				//Zerogarren laukira heltzeko behar baino gehiago ateratzen bada dadoan, onartuko dugu
+				//Zerogarren laukian ondo erantzuten badu, jokoa irabaziko du txanda daukan jokalariak
 				if(egungoZutabea == 0 && erantzuna == "zuzena"){
 					
 					garaileaDa	= true;
@@ -95,18 +94,19 @@ public class Tableroa {
 			}
 			
 			
-			else{
+			else{// Pasilloaren barnean dago, zero punturekin eta zirkulura mugitzeko asmoarekin
 				
 				int mugitzekoFalta	=  egungoZutabea + pAteratakoa - 6;
 				
-				//Positiboa edo zero bada, zirkulura ailegatu dela esan nahi du
+				// Positiboa edo zero bada, zirkulura ailegatu dela esan nahi du
 				if(mugitzekoFalta >= 0){
-					egungoZutabea		=	6; //Pasilloaren azkenengo laukitxora mugitzen, bertan zirkuluaren lotura baitago
-					hasierakoPasilloan	=	false;
+					egungoZutabea		=	6; // Pasilloaren azkenengo laukitxora mugitzen, bertan zirkuluaren lotura baitago
+					hasierakoPasilloan	=	false;	// Zirkuluan dagoela adierazten du
 					egungoZutabea		=
 							((LaukiGaztaduna)this.lehenengoBidea[egungoErrenkada][egungoZutabea]).getZutabeaZirkuluan();
 				
-					if(mugitzekoFalta > 0){
+					if(mugitzekoFalta > 0){/*Zirkulura mugitzeko beharrezkoa den zenbaki handiagoa ateratzen badu
+												falta zaiona zirkuluan mugitzeko balioko du */
 						
 						egungoZutabea		= this.mugituZirkuluan(egungoZutabea, mugitzekoFalta);
 						
@@ -114,39 +114,15 @@ public class Tableroa {
 					
 				}
 				
-				//Negatiboa da, beraz, "hasiera" eta "gaztatxo" laukien artean (0 < egungoZutabea < 6)
-				else{
+				else{ //Negatiboa da, beraz, "hasiera" eta "gaztatxo" laukien artean (0 < egungoZutabea < 6)
 					
 					egungoZutabea =	egungoZutabea + pAteratakoa;
 				}
 
-			
-			/*
-			else{
-				
-				while (kont < pAteratakoa && hasierakoPasilloan){
-				
-					kont++;
-					egungoZutabea++;
-					if( this.lehenengoBidea[egungoErrenkada][egungoZutabea] instanceof LaukiGaztaduna){
-						
-						hasierakoPasilloan	= false;
-						//downcasting-a erabiliz
-						egungoZutabea		= 
-								((LaukiGaztaduna)this.lehenengoBidea[egungoErrenkada][egungoZutabea]).getZutabeaZirkuluan();
-					}
-				}
-				
-				if (kont < pAteratakoa){ //zirkulura pasatu da eta oraindik mugitu behar da
-					int zenbatMugitu	= pAteratakoa - kont;
-					egungoZutabea		= this.mugituZirkuluan(egungoZutabea,zenbatMugitu);
-					
-				}
-				*/
 				
 				//Mugimengua guztiz burutu dela, galdera eskatzeko momentua da
 				
-				if(!hasierakoPasilloan){
+				if(!hasierakoPasilloan){ // Tableroaren atalaren arabera, koordenatu desberdinak behar dira galdera eskatzeko
 					
 					erantzuna		= this.bigarrenBidea[egungoZutabea].galderaFormulatu();
 	
@@ -165,7 +141,7 @@ public class Tableroa {
 			
 		}
 				 
-				
+		// Txandaren emaitzak aldaketak "kutxa" batean sartuko dira		
 		JokalariaAldaketak aldaketak = null;
 		boolean	txandarenEmaitza = (erantzuna != "okerra");
 		aldaketak	= new JokalariaAldaketak(hasierakoPasilloan, egungoErrenkada, egungoZutabea, txandarenEmaitza, garaileaDa);
@@ -185,7 +161,7 @@ public class Tableroa {
 		}
 		int	posizioBerria	= pEgungoPosizioa;
 		if (noraMugitu.equals("aurrera")){
-			// ponerlo en el powerpoint el fallo del if y no en case
+			
 			posizioBerria	= posizioBerria + pZenbatMugitu;
 			if (posizioBerria > 41){
 				
@@ -219,120 +195,115 @@ public class Tableroa {
 			catch (MugimenduDesegokiaException e){
 				e.inprimatu();
 			}
-	}while (!kontrolpean);
+		}while (!kontrolpean);
 		
 		return erantzuna;
 	}
 	
 	
-		/*public void setErrenkadaKop(int pErrenkadaKop){
-	        this.errenkadaKop=pErrenkadaKop;
+	private void gehituHasieranLaukia(Laukia pLaukia) {
+	 //Salbuespen if jokalari kop 6 edo 2
+	    this.lehenengoBidea[this.errenkadaKop][this.zutabeKop]=pLaukia;
+	    if(this.zutabeKop==6){
+	       this.errenkadaKop++;
+	       this.zutabeKop=0;
 	    }
-	    public void setZutabeKop(int pZutabeKop){
-	        this.zutabeKop=pZutabeKop;
+	    else{
+	    	
+	    	this.zutabeKop++;}//TODO
 	    }
-	    public void setPosizioZirkuluan(int pPosizioZirkuluan){
-	        this.posizioZirkuluan=pPosizioZirkuluan;
-	    }*/
+	private void gehituZirkuluanLaukia(Laukia pLaukia) {
+		//Salbuespen if jokalari kop 6 edo 2
+		this.bigarrenBidea[this.posizioZirkuluan]=pLaukia;
+		this.posizioZirkuluan++;
+		
+	}
 	
-	    private void gehituHasieranLaukia(Laukia pLaukia) {
-	        //Salbuespen if jokalari kop 6 edo 2
-	        this.lehenengoBidea[this.errenkadaKop][this.zutabeKop]=pLaukia;
-	        if(this.zutabeKop==6){
-	        	this.errenkadaKop++;
-	        	this.zutabeKop=0;
-	        }
-	        else{
-	        this.zutabeKop++;}//TODO
-	    }
-	    private void gehituZirkuluanLaukia(Laukia pLaukia) {
-	        //Salbuespen if jokalari kop 6 edo 2
-	        this.bigarrenBidea[this.posizioZirkuluan]=pLaukia;
-	        this.posizioZirkuluan++;//TODO
-	    }
-	    public void tableroaHasieratu(){
-	    	//LaukiGaztaduna(String pKolorea, int pErrenkada, int pZutabeaZirkuluan){
-	    	int kont = 0;
-	    	String[] zatiak=new String[3];
-	    	String koloreaGaztatxo=null;
-	    	String errenkada=null;
-	    	String zutabeaZirkulua=null;
-	    	int errenkadaInt=0;
-	    	int zutabeaZirkuluanInt=0;
-			Laukia laukiBat=null;
-			InputStream fitx=null;
-			InputStreamReader in=null;
-			Scanner sc = null;
-			String kolorea=null;
+    public void tableroaHasieratu(){
+    
+    	int kont = 0;
+    	String[] zatiak=new String[3];
+    	String koloreaGaztatxo=null;
+    	String errenkada=null;
+    	String zutabeaZirkulua=null;
+    	int errenkadaInt=0;
+    	int zutabeaZirkuluanInt=0;
+		Laukia laukiBat=null;
+		InputStream fitx=null;
+		InputStreamReader in=null;
+		Scanner sc = null;
+		String kolorea=null;
+		
+		String fitxategia="Fitxategiak"+File.separator+"BarrukoTablero.txt";
+				
+		fitx = this.getClass().getClassLoader().getResourceAsStream(fitxategia);
+		in = new InputStreamReader(fitx);			
+		sc = new Scanner(in);
+		// Pasilloak/Matrizea hasieratzen
+		while (sc.hasNextLine()){
 			
-			String fitxategia="Fitxategiak"+File.separator+"BarrukoTablero.txt";
-					
-			fitx = this.getClass().getClassLoader().getResourceAsStream(fitxategia);
-			in = new InputStreamReader(fitx);			
-			sc = new Scanner(in);
+			kolorea = sc.nextLine();
+			// LaukiGaztadunak badira
+			if (kont==6 || kont==13 || kont==20 || kont==27 || kont==34 || kont==41){
+				zatiak=kolorea.split("#");
+				koloreaGaztatxo = zatiak[0];
+				errenkada = zatiak[1];
+				zutabeaZirkulua=zatiak[2];
+				errenkadaInt = Integer.parseInt(errenkada);
+				zutabeaZirkuluanInt = Integer.parseInt(zutabeaZirkulua);
+				laukiBat= new LaukiGaztaduna(koloreaGaztatxo,errenkadaInt,zutabeaZirkuluanInt);
+			}
+			// Lauki normalak badira
+			else{
 			
-			while (sc.hasNextLine()){
-				
-				kolorea = sc.nextLine();
-				
-				if (kont==6 || kont==13 || kont==20 || kont==27 || kont==34 || kont==41){
-					zatiak=kolorea.split("#");
-					koloreaGaztatxo = zatiak[0];
-					errenkada = zatiak[1];
-					zutabeaZirkulua=zatiak[2];
-					errenkadaInt = Integer.parseInt(errenkada);
-					zutabeaZirkuluanInt = Integer.parseInt(zutabeaZirkulua);
-					laukiBat= new LaukiGaztaduna(koloreaGaztatxo,errenkadaInt,zutabeaZirkuluanInt);
-				}
-				else{
-				
-				laukiBat=new Laukia(kolorea);
-				
-				}
+			laukiBat=new Laukia(kolorea);
+			
+			}
 
-				this.gehituHasieranLaukia(laukiBat);
-				
-				kont++;
-			}
-			sc.close();
-			kont=0;
-			fitxategia="Fitxategiak"+File.separator+"KanpokoTablero.txt";
+			this.gehituHasieranLaukia(laukiBat);
 			
-			fitx = this.getClass().getClassLoader().getResourceAsStream(fitxategia);
-			in = new InputStreamReader(fitx);			
-			sc = new Scanner(in);
+			kont++;
+		}
+		sc.close();
+		kont=0;
+		fitxategia="Fitxategiak"+File.separator+"KanpokoTablero.txt";
+		
+		fitx = this.getClass().getClassLoader().getResourceAsStream(fitxategia);
+		in = new InputStreamReader(fitx);			
+		sc = new Scanner(in);
+		
+		// Zirkulua/Arraya hasieratzen
+		while (sc.hasNextLine()){
 			
-			while (sc.hasNextLine()){
+			kolorea = sc.nextLine();
+			
+			if (kont==6 || kont==13 || kont==20 || kont==27 || kont==34 || kont==41){
 				
-				kolorea = sc.nextLine();
+				zatiak=kolorea.split("#");
+				koloreaGaztatxo = zatiak[0];
+				errenkada = zatiak[1];
+				zutabeaZirkulua=zatiak[2];
+				errenkadaInt = Integer.parseInt(errenkada);
+				zutabeaZirkuluanInt = Integer.parseInt(zutabeaZirkulua);
+				laukiBat= new LaukiGaztaduna(koloreaGaztatxo,errenkadaInt,zutabeaZirkuluanInt);
 				
-				if (kont==6 || kont==13 || kont==20 || kont==27 || kont==34 || kont==41){
-					
-					zatiak=kolorea.split("#");
-					koloreaGaztatxo = zatiak[0];
-					errenkada = zatiak[1];
-					zutabeaZirkulua=zatiak[2];
-					errenkadaInt = Integer.parseInt(errenkada);
-					zutabeaZirkuluanInt = Integer.parseInt(zutabeaZirkulua);
-					laukiBat= new LaukiGaztaduna(koloreaGaztatxo,errenkadaInt,zutabeaZirkuluanInt);
-					
-				}
-				else{
-				
-				laukiBat=new Laukia(kolorea);
-				
-				
-				}
-				
-				this.gehituZirkuluanLaukia(laukiBat);
-				
-				kont++;
 			}
-			sc.close();
-	    }
-	    public void erreseteatu(){
-	    	Tableroa.nireTableroa=null;
-	    }
+			else{
+			
+			laukiBat=new Laukia(kolorea);
+			
+			
+			}
+			
+			this.gehituZirkuluanLaukia(laukiBat);
+			
+			kont++;
+		}
+		sc.close();
+    }
+    public void erreseteatu(){
+    	Tableroa.nireTableroa=null;
+    }
 
 }
 	
