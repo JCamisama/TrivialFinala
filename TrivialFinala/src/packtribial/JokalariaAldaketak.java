bfc980c1f6f9c2395c59d03a181632df91a9075b
GySyX <GySyX@LAPTOP-K6JCCTH2>
2019-06-05 11:11:47 +0200

    Comments JokalariaAldaketak

--- TrivialFinala/src/packtribial/JokalariaAldaketak.java
+++ TrivialFinala/src/packtribial/JokalariaAldaketak.java
@@ -1,19 +1,22 @@
 package packtribial;
 
 public class JokalariaAldaketak {
-	private boolean hasierakoPasilloan;
-	private int posizioaErrenkada;
-	private int posizioaZutabea;
-	private boolean txandarenEmaitza;
-	private boolean partidaAmaituDa;
+	
+	private boolean hasierakoPasilloan; 
+	private int posizioaErrenkada;			// Pasilloren batean badago jakiteko
+	private int posizioaZutabea;			// Zirkuluan edo pasillo batean daukan posizioa
+	private boolean txandarenEmaitza;		// True izanez gero, jokalariak txanda errepikatuko du
+	private boolean partidaAmaituDa;		// Txandaren amaieran jokalaria partida irabazi duen adierazteko
 	
 	public JokalariaAldaketak(boolean pHasierakoPasilloan, int pPosizioaErrenkada, int pPosizioaZutabea, boolean pTxandarenEmaitza,
 			boolean pPartidaAmaituDa){
+		
 		this.hasierakoPasilloan=pHasierakoPasilloan;
 		this.posizioaErrenkada=pPosizioaErrenkada;
 		this.posizioaZutabea=pPosizioaZutabea;
 		this.txandarenEmaitza=pTxandarenEmaitza;
 		this.partidaAmaituDa=pPartidaAmaituDa;
+		
 	}
 	public boolean getHasierakoPasilloa(){
 		return this.hasierakoPasilloan;
